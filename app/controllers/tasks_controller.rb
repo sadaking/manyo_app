a3abaebeab21de10d29d102e82bbf45945041e0b
sadaking <sichirigahama.37@gmail.com>
2020-05-19 16:42:51 +0900

    詳細機能作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,24 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to new_task_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name, :task_details, :deadline, :status, :priority, :author)
   end
 end
