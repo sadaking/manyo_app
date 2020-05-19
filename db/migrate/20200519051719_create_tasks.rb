6b3d5c678791bed5653f0304d2d514c5f29a46cf
sadaking <sichirigahama.37@gmail.com>
2020-05-19 17:34:28 +0900

    null: falseの追加

--- db/migrate/20200519051719_create_tasks.rb
+++ db/migrate/20200519051719_create_tasks.rb
@@ -1,12 +1,12 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :task_name
-      t.string :task_details
-      t.string :deadline
-      t.string :status
-      t.string :priority
-      t.string :author
+      t.string :task_name, null: false
+      t.string :task_details, null: false
+      t.string :deadline, null: false
+      t.string :status, null: false
+      t.string :priority, null: false
+      t.string :author, null: false
     end
   end
 end
