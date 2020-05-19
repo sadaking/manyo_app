d1f8108d732365d83684ef9ecd0ecace2611690b
sadaking <sichirigahama.37@gmail.com>
2020-05-19 17:31:45 +0900

    バリテーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,8 @@
 class Task < ApplicationRecord
+  validates :task_name, presence: true
+  validates :task_details, presence: true
+  validates :deadline, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
+  validates :author, presence: true
 end
