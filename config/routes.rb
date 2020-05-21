464d3bfe152e51ffc801bbbd7ff6813bf09f25b3
sadaking <sichirigahama.37@gmail.com>
2020-05-21 09:55:09 +0900

    ログインログアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/new'
   root to: 'tasks#index'
   resources :tasks
+  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
