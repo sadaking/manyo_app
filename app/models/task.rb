763a7f29d6b5fa58b5806797b6913d026966dfa7
sadaking <sichirigahama.37@gmail.com>
2020-05-21 09:56:32 +0900

    rspecを行う上での調整

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,3 @@
 class Task < ApplicationRecord
-  validates :task_name, presence: true
-  validates :task_details, presence: true
-  validates :deadline, presence: true
-  validates :status, presence: true
-  validates :priority, presence: true
-  validates :author, presence: true
+  belongs_to :user
 end
