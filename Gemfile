67127cfec13f2aad32421a334a4331c763483a55
sadaking <sichirigahama.37@gmail.com>
2020-05-21 09:48:58 +0900

    rspecの導入

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ group :development, :test do
   gem 'pry-rails'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'rspec-rails', '~> 3.8'
+  gem 'factory_bot_rails'
 end
 
 group :development do
@@ -54,11 +56,8 @@ group :development do
 end
 
 group :test do
-  # Adds support for Capybara system testing and selenium driver
-  gem 'capybara', '>= 2.15'
-  gem 'selenium-webdriver'
-  # Easy installation and use of chromedriver to run system tests with Chrome
-  gem 'chromedriver-helper'
+  gem 'capybara'
+  gem 'webdrivers'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ spec/system/task_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'タスク管理機能', type: :system do
+  describe 'タスク一覧画面' do
+    context 'タスクを作成した場合' do
+      # テストコードを it '~' do end ブロックの中に記載する
+      it '作成済みのタスクが表示される' do
+        # テストで使用するためのタスクを作成
+        task = FactoryBot.create(:task, title: 'task')
+        # タスク一覧ページに遷移
+        visit tasks_path
+        # visitした（遷移した）page（タスク一覧ページ）に「task」という文字列が
+        # have_contentされているか。（含まれているか。）ということをexpectする（確認・期待する）
+        expect(page).to have_content 'task'
+        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
+      end
+    end
+  end
+
+
+  describe 'タスク登録画面' do
+    context '必要項目を入力して、createボタンを押した場合' do
+      it 'データが保存される'
+      end
+    end
+
+  describe 'タスク詳細画面' do
+     context '任意のタスク詳細画面に遷移した場合' do
+       it '該当タスクの内容が表示されたページに遷移する'
+      end
+  end
+end
